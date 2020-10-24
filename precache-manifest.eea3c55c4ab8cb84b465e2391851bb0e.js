b7ef3eabd27d323401903a25063f8ead88dab1f7
Tareq <tareqtherocker@gmail.com>
2020-10-24 20:46:34 +0600

    Updates

---
+++ precache-manifest.eea3c55c4ab8cb84b465e2391851bb0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30967b460f049722fc93d578c15a8aaa",
+    "url": "/redux-posts-and-comments/index.html"
+  },
+  {
+    "revision": "116fdad9e7a0fcda5f24",
+    "url": "/redux-posts-and-comments/static/css/main.1e709b65.chunk.css"
+  },
+  {
+    "revision": "c7d18564db3019f4069e",
+    "url": "/redux-posts-and-comments/static/js/2.3f4294ab.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/redux-posts-and-comments/static/js/2.3f4294ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "116fdad9e7a0fcda5f24",
+    "url": "/redux-posts-and-comments/static/js/main.0e2db12f.chunk.js"
+  },
+  {
+    "revision": "0939bb46400b4fd995cf",
+    "url": "/redux-posts-and-comments/static/js/runtime-main.32805b6d.js"
+  }
+]);
